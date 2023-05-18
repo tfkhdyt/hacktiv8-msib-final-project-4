0bfd033b9d6b0a3cb677cf658bafbd521fa58495
tfkhdyt <tfkhdyt@proton.me>
2023-05-18 14:05:42 +0700

    feat(postgres): setup admin seeding

--- database/postgres.go
+++ database/postgres.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"errors"
 	"hacktiv8-msib-final-project-4/config"
+	"hacktiv8-msib-final-project-4/entity"
 	"hacktiv8-msib-final-project-4/pkg/errs"
 	"log"
 
@@ -13,11 +15,32 @@ var (
 	err error
 )
 
+func seedAdmin() {
+	admin := &entity.User{
+		FullName: "admin",
+		Email:    "admin@hacktiv8.com",
+		Password: "admin123",
+		Role:     "admin",
+		Balance:  0,
+	}
+	errs.CheckErr(admin.HashPassword())
+
+	errs.CheckErr(db.Create(admin).Error)
+
+	log.Println("Admin account seed success!")
+}
+
 func init() {
 	db, err = gorm.Open(config.GetDBConfig())
 	errs.CheckErr(err)
 
-	errs.CheckErr(db.AutoMigrate())
+	errs.CheckErr(db.AutoMigrate(&entity.User{}))
+
+	if db.Migrator().HasTable(&entity.User{}) {
+		if err := db.First(&entity.User{}).Error; errors.Is(err, gorm.ErrRecordNotFound) {
+			seedAdmin()
+		}
+	}
 
 	log.Println("Connected to DB!")
 }
