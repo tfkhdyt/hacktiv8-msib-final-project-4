9827eaeaf7b6203bb6acba131e32e4300293e7a0
tfkhdyt <tfkhdyt@proton.me>
2023-05-19 14:03:04 +0700

    feat(service): add auth service for middleware

---
+++ service/auth_service.go
@@ -0,0 +1,63 @@
+package service
+
+import (
+	"hacktiv8-msib-final-project-4/entity"
+	"hacktiv8-msib-final-project-4/pkg/errs"
+	"hacktiv8-msib-final-project-4/repository/userrepository"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthService interface {
+	Authentication() gin.HandlerFunc
+	AdminAuthorization() gin.HandlerFunc
+}
+
+type authService struct {
+	userRepo userrepository.UserRepository
+}
+
+func NewAuthService(userRepo userrepository.UserRepository) AuthService {
+	return &authService{userRepo}
+}
+
+func (a *authService) Authentication() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		bearerToken := ctx.GetHeader("Authorization")
+
+		var user entity.User
+
+		if err := user.ValidateToken(bearerToken); err != nil {
+			ctx.AbortWithStatusJSON(err.StatusCode(), err)
+			return
+		}
+
+		result, err := a.userRepo.GetUserByID(user.ID)
+		if err != nil {
+			ctx.AbortWithStatusJSON(err.StatusCode(), err)
+			return
+		}
+
+		ctx.Set("userData", result)
+		ctx.Next()
+	}
+}
+
+func (a *authService) AdminAuthorization() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userData, ok := ctx.MustGet("userData").(*entity.User)
+		if !ok {
+			newError := errs.NewBadRequest("Failed to get user data")
+			ctx.AbortWithStatusJSON(newError.StatusCode(), newError)
+			return
+		}
+
+		if userData.Role != "admin" {
+			newError := errs.NewUnauthorized("You're not authorized to access this endpoint")
+			ctx.AbortWithStatusJSON(newError.StatusCode(), newError)
+			return
+		}
+
+		ctx.Next()
+	}
+}
