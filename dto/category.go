2b4bb54cec07c9478b0c50086c01d84230589dc1
tfkhdyt <tfkhdyt@proton.me>
2023-05-19 19:10:19 +0700

    feat(category): add "create category" feature

---
+++ dto/category.go
@@ -0,0 +1,24 @@
+package dto
+
+import (
+	"hacktiv8-msib-final-project-4/entity"
+	"time"
+)
+
+type CreateCategoryRequest struct {
+	Type string `json:"type" binding:"required"`
+}
+
+func (c *CreateCategoryRequest) ToEntity() *entity.Category {
+	return &entity.Category{
+		Type:              c.Type,
+		SoldProductAmount: 0,
+	}
+}
+
+type CreateCategoryResponse struct {
+	ID                uint      `json:"id"`
+	Type              string    `json:"type"`
+	SoldProductAmount uint      `json:"sold_product_amount"`
+	CreatedAt         time.Time `json:"created_at"`
+}
--- handler/app.go
+++ handler/app.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"hacktiv8-msib-final-project-4/database"
 	"hacktiv8-msib-final-project-4/handler/httphandler"
+	"hacktiv8-msib-final-project-4/repository/categoryrepository/categorypg"
 	"hacktiv8-msib-final-project-4/repository/userrepository/userpg"
 	"hacktiv8-msib-final-project-4/service"
 	"log"
@@ -25,11 +26,17 @@ func StartApp() {
 	userService := service.NewUserService(userRepo)
 	userHandler := httphandler.NewUserHandler(userService)
 
+	categoryRepo := categorypg.NewCategoryPG(db)
+	categoryService := service.NewCategoryService(categoryRepo)
+	categoryHandler := httphandler.NewCategoryHandler(categoryService)
+
 	authService := service.NewAuthService(userRepo)
 
 	r.POST("/users/register", userHandler.Register)
 	r.POST("/users/login", userHandler.Login)
 	r.PATCH("/users/topup", authService.Authentication(), userHandler.TopUp)
+
+	r.POST("/categories", authService.Authentication(), authService.AdminAuthorization(), categoryHandler.CreateCategory)
 
 	log.Fatalln(r.Run(":" + port))
 }
