30c857dbf0728a6ade664a5eb2e1fa1baaa109e8
tfkhdyt <tfkhdyt@proton.me>
2023-05-23 07:16:21 +0700

    feat(product): add "update product" feature

--- service/product_service.go
+++ service/product_service.go
@@ -5,11 +5,24 @@ import (
 	"hacktiv8-msib-final-project-4/pkg/errs"
 	"hacktiv8-msib-final-project-4/repository/categoryrepository"
 	"hacktiv8-msib-final-project-4/repository/productrepository"
+
+	"github.com/leekchan/accounting"
+)
+
+var (
+	lc = accounting.LocaleInfo["IDR"]
+	ac = accounting.Accounting{
+		Symbol:    "Rp",
+		Precision: 2,
+		Thousand:  lc.ThouSep,
+		Decimal:   lc.DecSep,
+	}
 )
 
 type ProductService interface {
 	CreateProduct(payload *dto.CreateProductRequest) (*dto.CreateProductResponse, errs.MessageErr)
 	GetAllProducts() ([]dto.GetAllProductsResponse, errs.MessageErr)
+	UpdateProduct(id uint, payload *dto.UpdateProductRequest) (*dto.UpdateProductResponse, errs.MessageErr)
 }
 
 type productService struct {
@@ -24,8 +37,7 @@ func NewProductService(productRepo productrepository.ProductRepository, category
 func (p *productService) CreateProduct(payload *dto.CreateProductRequest) (*dto.CreateProductResponse, errs.MessageErr) {
 	product := payload.ToEntity()
 
-	_, checkCategoryErr := p.categoryRepo.GetCategoryByID(product.CategoryID)
-	if checkCategoryErr != nil {
+	if _, checkCategoryErr := p.categoryRepo.GetCategoryByID(product.CategoryID); checkCategoryErr != nil {
 		return nil, checkCategoryErr
 	}
 
@@ -62,6 +74,38 @@ func (p *productService) GetAllProducts() ([]dto.GetAllProductsResponse, errs.Me
 			CategoryID: product.CategoryID,
 			CreatedAt:  product.CreatedAt,
 		})
+	}
+
+	return response, nil
+}
+
+func (p *productService) UpdateProduct(id uint, payload *dto.UpdateProductRequest) (*dto.UpdateProductResponse, errs.MessageErr) {
+	product := payload.ToEntity()
+
+	if _, checkCategoryErr := p.categoryRepo.GetCategoryByID(product.CategoryID); checkCategoryErr != nil {
+		return nil, checkCategoryErr
+	}
+
+	oldProduct, err := p.productRepo.GetProductByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	updatedProduct, errUpdate := p.productRepo.UpdateProduct(oldProduct, product)
+	if errUpdate != nil {
+		return nil, errUpdate
+	}
+
+	response := &dto.UpdateProductResponse{
+		Product: dto.ProductDataWithCategoryID{
+			ID:         updatedProduct.ID,
+			Title:      updatedProduct.Title,
+			Price:      ac.FormatMoney(updatedProduct.Price),
+			Stock:      updatedProduct.Stock,
+			CategoryID: updatedProduct.CategoryID,
+			CreatedAt:  updatedProduct.CreatedAt,
+			UpdatedAt:  updatedProduct.UpdatedAt,
+		},
 	}
 
 	return response, nil
