b5b4be80276b884e6f986a2070756e33a8af1b7a
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-06-11 10:36:44 +0800

    fix: dto transaction history

--- dto/transaction_history.go
+++ dto/transaction_history.go
@@ -31,7 +31,7 @@ type GetTransactionsByUserIDResponse struct {
 	UserID     uint                                     `json:"user_id"`
 	Quantity   uint                                     `json:"quantity"`
 	TotalPrice uint                                     `json:"total_price"`
-	Product    ProductDataWithCategoryIDAndIntegerPrice `json:"Product"`
+	Product    ProductDataWithCategoryIDAndIntegerPrice `json:"product"`
 }
 
 type GetUserTransactionsResponse struct {
